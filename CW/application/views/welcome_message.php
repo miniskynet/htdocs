3b5b897b9553d40a420daa4d235609bdf4d9adab
Desira Wijesundara <desirasenal@gmail.com>
2024-04-19 21:53:48 +0530

    CW : Create welcome page : WIP

--- CW/application/views/welcome_message.php
+++ CW/application/views/welcome_message.php
@@ -1,99 +1,113 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
 	<title>Welcome to CodeIgniter</title>
 
 	<style type="text/css">
+		body {
+			background-color: #202225;
+			margin: 40px;
+			font: 13px/20px normal Helvetica, Arial, sans-serif;
+			color: #4F5155;
+		}
 
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
+		a {
+			color: #003399;
+			background-color: transparent;
+			font-weight: normal;
+		}
 
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
+		a:hover {
+			color: #97310e;
+		}
 
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-		text-decoration: none;
-	}
+		#container {
+			margin: 10px;
+			display: flex;
+			justify-content: flex-end;
+		}
 
-	a:hover {
-		color: #97310e;
-	}
+		#nav {
+			background-color: #202225;
+			color: #fff;
+			padding: 10px 20px;
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			font-family: 'Arial', sans-serif;
+			border-bottom: 3px solid #5865F2;
+		}
 
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
+		h1, h2 {
+			color: #fff;
+		}
 
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
+		input, h2 {
+			margin: 10px;
+		}
 
-	#body {
-		margin: 0 15px 0 15px;
-		min-height: 96px;
-	}
+		input[type="text"],
+		input[type="password"],
+		input[type="email"] {
+			width: 50%;
+			padding: 12px 20px;
+			margin-bottom: 15px;
+			border: 1px solid #ccc;
+			border-radius: 4px;
+		}
 
-	p {
-		margin: 0 0 10px;
-		padding:0;
-	}
+		input[type="submit"] {
+			color: #ffffff;
+			background-color: #015546;
+			padding: 12px 20px;
+		}
 
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
+		#section {
+			width: 50%;
+		}
 
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
+		#subsection{
+			padding-top: 10px;
+			padding-bottom: 10px;
+			border-bottom: 3px solid #89f258;
+		}
+
 	</style>
 </head>
 <body>
 
+<div id="nav">
+	<h1>Welcome to Gamers United!</h1>
+</div>
+
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<div id="section">
+		<div id="subsection">
+			<form>
+				<h2>Login or Sign-up</h2><br>
+				<input type="text" id="usernameLogin" name="usernameLogin" placeholder="Username">
+				<input type="password" id="passLogin" name="passLogin" placeholder="Password"><br>
+				<input type="submit" value="Login"><br>
+			</form>
+		</div>
+		<div id="subsection">
+			<form>
+				<input type="text" id="fname" name="fname" placeholder="First Name"><br>
+				<input type="text" id="lname" name="lname" placeholder="Last Name"><br>
+				<input type="text" id="usernameSignup" name="usernameSignup" placeholder="Username"><br>
+				<input type="password" id="passSignup" name="passSignup" placeholder="Password"><br>
+				<input type="password" id="passConfirm" name="pass" placeholder="Password Confirmation"><br>
+				<input type="email" id="pass" name="pass" placeholder="Email"><br>
+				<input type="submit" value="Signup">
+			</form>
+		</div>
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="userguide3/">User Guide</a>.</p>
 	</div>
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
 
 </body>
